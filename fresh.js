d50cb491d7a8cea39272a22327d26b6bc7105d46
Hunter Ball <35305713+HunterBall@users.noreply.github.com>
2019-03-28 09:54:58 -0600

    Add files via upload

---
+++ fresh.js
@@ -0,0 +1,7 @@
+$('.nav-trigger').click(function() {
+	$('.nav.side').toggleClass("active");
+	$('.nav-trigger').toggleClass("hidden");
+});
+$('.fresh-modal-trigger').click(function() {
+	$('.fresh-modal').toggleClass("active");
+});
