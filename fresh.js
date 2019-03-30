730881e08f9efff3769269ff8440685db2303b5a
Hunter Ball <35305713+HunterBall@users.noreply.github.com>
2019-03-30 00:20:27 -0600

    Delete fresh.js

--- fresh.js
+++
@@ -1,7 +0,0 @@
-$('.nav-trigger').click(function() {
-	$('.nav.side').toggleClass("active");
-	$('.nav-trigger').toggleClass("hidden");
-});
-$('.fresh-modal-trigger').click(function() {
-	$('.fresh-modal').toggleClass("active");
-});
