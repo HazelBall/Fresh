69278468cf235eea70ad20f3b78aeeb095fc9e34
Hunter Ball <35305713+HunterBall@users.noreply.github.com>
2019-05-21 09:13:18 -0600

    Update fresh.js

--- fresh.js
+++ fresh.js
@@ -5,3 +5,8 @@ $('.nav-trigger').click(function() {
 $('.fresh-modal-trigger').click(function() {
 	$('.fresh-modal').toggleClass("active");
 });
+window.onclick = function(event) {
+  if (event.target == $('.fresh-modal-trigger') || event.target == $('.nav-trigger')) {
+    modal.style.display = "none";
+  }
+}
